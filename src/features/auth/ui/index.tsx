a4b28044b09609e87dc118c5e2165163b0bb1054
Kramarich <karen.avakov2@gmail.com>
2025-12-28 02:25:17 +0300

    feat(mail): изменен способ отправки почты теперь это resend

--- src/features/auth/ui/index.tsx
+++ src/features/auth/ui/index.tsx
@@ -1,15 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "next-auth/react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
-import { Alert } from "@/shared/ui/alert";
 import { Button } from "@/shared/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/shared/ui/form";
+import GithubIcon from "@/shared/ui/github-icon";
+import { GoogleIcon } from "@/shared/ui/google-icon";
 import { Input } from "@/shared/ui/input";
+import { YandexIcon } from "@/shared/ui/yandex-icon";
 
 const MagicLinkSchema = z.object({
   email: z.email({ message: "Введите корректный email" }),
@@ -23,6 +26,12 @@ export function AuthCard() {
     resolver: zodResolver(MagicLinkSchema),
     defaultValues: { email: "" },
   });
+
+  useEffect(() => {
+    if (errorMessage != null) {
+      toast.error(errorMessage);
+    }
+  }, [errorMessage]);
 
   const onSubmit = async (values: z.infer<typeof MagicLinkSchema>) => {
     setErrorMessage(null);
@@ -42,9 +51,9 @@ export function AuthCard() {
   if (isSent) {
     return (
       <div className="space-y-4 text-center">
-        <h2 className="text-xl font-bold">Проверьте почту!</h2>
+        <h2 className="text-xl font-bold">Проверьте почту.</h2>
         <p className="text-muted-foreground">
-          Мы отправили ссылку на <b>{form.getValues("email")}</b>.
+          Мы отправили ссылку на <b className="italic">{form.getValues("email")}</b>.
         </p>
         <Button variant="outline" onClick={() => setIsSent(false)}>
           Ввести другой Email
@@ -54,45 +63,47 @@ export function AuthCard() {
   }
 
   return (
-    <div className="w-full max-w-md space-y-6">
+    <div className="flex max-w-md flex-col items-center justify-center gap-6">
+      <div className="flex items-center justify-center gap-4">
+        <Button className="cursor-pointer" onClick={() => signIn("github")}>
+          <GithubIcon /> GitHub
+        </Button>
+        <Button className="cursor-pointer" onClick={() => signIn("google")}>
+          <GoogleIcon /> Google
+        </Button>
+        <Button className="cursor-pointer" onClick={() => signIn("yandex")}>
+          <YandexIcon /> Yandex
+        </Button>
+      </div>
+      <div className="relative w-full">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background text-muted-foreground px-2">или</span>
+        </div>
+      </div>
+
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          {errorMessage != null && <Alert variant="destructive">{errorMessage}</Alert>}
-
           <FormField
             control={form.control}
             name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Email</FormLabel>
+                <FormLabel>Введите Email</FormLabel>
                 <FormControl>
-                  <Input placeholder="name@company.com" {...field} />
+                  <Input placeholder="example@gmail.com" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit" className="w-full">
+          <Button type="submit" className="w-full cursor-pointer">
             Войти по Email
           </Button>
         </form>
       </Form>
-
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background text-muted-foreground px-2">Или через соцсети</span>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 gap-2">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("google")}>Google</Button>
-        {/* <Button onClick={() => signIn("gitlab")}>GitLab</Button> */}
-        <Button onClick={() => signIn("yandex")}>Yandex</Button>
-      </div>
     </div>
   );
 }
