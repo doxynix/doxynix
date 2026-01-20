ac9f18c95e3b928bc70a7fc1f4c366283bdcc440
Kramarich0 <karen.avakov2@gmail.com>
2026-01-21 00:01:53 +0300

    chore(landing): небольшое исправление

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 import { withAxiom } from "next-axiom";
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
@@ -175,4 +172,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withNextIntl(withAxiom(bundleAnalyzer(nextConfig)));
+export default withAxiom(bundleAnalyzer(nextConfig));
