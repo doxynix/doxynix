c5235631e5da0105cde03669a75474b92710c46f
Kramarich <147999794+Kramarich0@users.noreply.github.com>
2026-02-07 21:41:25 +0300

    fix(trigger): попытка исправления ошибок trigger.dev (#258)

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,12 +1,10 @@
+import { execSync } from "child_process";
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
   project: "proj_whmkduzdqfzcgoyuhdyp",
   runtime: "node",
   logLevel: "log",
-  // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
-  // You can override this on an individual task.
-  // See https://trigger.dev/docs/runs/max-duration
   maxDuration: 3600,
   retries: {
     enabledInDev: true,
@@ -19,4 +17,25 @@ export default defineConfig({
     },
   },
   dirs: ["./src/trigger"],
+
+  build: {
+    extensions: [
+      {
+        name: "zenstack-generate",
+        onBuildStart: async () => {
+          console.log("🛠 ZenStack generating...");
+          try {
+            execSync("npx zenstack generate --schema prisma/schema.zmodel", {
+              stdio: "inherit",
+            });
+            console.log("✅ ZenStack generation successful!");
+          } catch (e) {
+            console.error("❌ ZenStack generation failed", e);
+            throw e;
+          }
+        },
+      },
+    ],
+    external: ["@prisma/client", ".prisma/client"],
+  },
 });
