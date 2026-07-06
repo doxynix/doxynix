d798218974a0f578cf8d6d23b3d62d8b179e03a4
Kramarich0 <karen.avakov2@gmail.com>
2026-07-06 14:57:19 +0300

    refactor: change openapi runtime to nodejs

--- src/app/api/openapi/route.ts
+++ src/app/api/openapi/route.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 
 import { IS_PROD } from "@/shared/constants/env.flags";
 
-import { appLogger } from "@/server/core/app-logger";
-
 import openApiDocument from "../../../../public/openapi.json";
 
 export const runtime = "edge";
@@ -16,7 +14,6 @@ export const GET = async () => {
       },
     });
   } catch (error) {
-    appLogger.error({ error, msg: "OpenAPI static serving error" });
     return NextResponse.json(
       {
         details: IS_PROD ? undefined : error instanceof Error ? error.message : String(error),
